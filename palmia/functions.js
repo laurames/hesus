6e9d1a20701cf31a1fdf197d7921f1aebc75342a
Carolina Rodriguez Torres <karolrodtorres@gmail.com>
2013-03-10 14:31:02 +0200

    the events hover adapted to touchscreen

--- palmia/functions.js
+++ palmia/functions.js
@@ -1,7 +1,7 @@
 /* Javascript document */
 
 $(function() {
-            	/* We'll have to change event into click */
+            	/* We'll have to change event into click 
                 $('#accordion > li').hover(
                     function () {
                         var $this = $(this);
@@ -17,7 +17,24 @@ $(function() {
                         $('.description',$this).stop(true,true).fadeOut(500);
                         $('.bgDescription',$this).stop(true,true).slideUp(700);
                     }
-                );
+                ); */
+
+                $('#accordion > li').on({
+                    mousedown: function () {
+                        var $this = $(this);
+                        $this.stop().animate({'width':'580px'},500);
+                        $('.heading',$this).stop(true,true).fadeOut();
+                        $('.bgDescription',$this).stop(true,true).slideDown(500);
+                        $('.description',$this).stop(true,true).fadeIn();
+                    },
+                    mouseup: function () {
+                        var $this = $(this);
+                        $this.stop().animate({'width':'215px'},1000);
+                        $('.heading',$this).stop(true,true).fadeIn();
+                        $('.description',$this).stop(true,true).fadeOut(500);
+                        $('.bgDescription',$this).stop(true,true).slideUp(700);
+                    }
+                });
 
            		$('li:contains("vegetable")').last().css('background-image', 'url(img/vegetables.jpg)');
            		$('li:contains("bread")').first().css('background-image', 'url(img/bread.jpg)');
